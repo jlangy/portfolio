8f29cdae2a8e79c2ab331738e9e73f012bd6ee85
Jon Langlois <jonathan-langlois@live.ca>
2020-04-05 13:05:29 -0700

    Introduction card
    What:
    
    1) Example of material-ui makeStyles
    2) Simple introduction card
    
    Why:
    
    1) Integrate css into components files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Avatar } from '@material-ui/core';
+import Introduction from './components/Introduction/Introduction'
 
 function App() {
   return (
     <div className="App">
-      <Avatar alt="Jon Langlois" src="/logo192.png" />
+      <Introduction />
     </div>
   );
 }
---
+++ src/components/Introduction/Introduction.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Card, CardContent, Avatar, Container } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  avatar: {
+    width: '100px !important',
+    height: '100px'
+  },
+  flexRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
+});
+
+function Introduction(props) {
+  const classes = useStyles();
+  return (
+    <Container>
+      <Card>
+        <CardContent className={classes.flexRow}>
+          <div className="introduction-text">
+            <h1>Jon Langlois</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur soluta eum, assumenda, totam minima dolore adipisci cumque reiciendis accusantium vero alias. Repudiandae eius doloremque qui sunt nisi, itaque a perferendis.</p>
+          </div>
+          <Avatar className={classes.avatar} src="./me.jpeg" alt="Jon Langlois" />
+        </CardContent>
+      </Card>
+    </Container>
+  );
+}
+
+export default Introduction;
