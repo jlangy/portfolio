6b8c4b4dab82c6f53a829d1ef5140ee185c28fea
Jon Langlois <jonathan-langlois@live.ca>
2020-04-13 13:54:12 -0700

    project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import ImageCarousel from './ImageCarousel'
 
@@ -13,6 +11,13 @@ const useStyles = makeStyles({
     margin: 'auto',
     marginTop: '100px'
   },
+  projectHeader: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  projectLink: {
+    fontSize: '1.5em'
+  }
 });
 
 export default function ProjectCard() {
@@ -26,22 +31,17 @@ export default function ProjectCard() {
         slides={['url(dog.jpeg)', 'url(dog2.jpeg)', 'url(dog3.jpeg)']}
       />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-          Lizard
+        <Typography gutterBottom variant="h4" component="h2">
+          <span className={classes.projectHeader}>
+            Lizard
+            <i className={classes.projectLink + " fab fa-github"}></i>
+          </span>
         </Typography>
         <Typography variant="body2" color="textSecondary" component="p">
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
     </Card>
   );
 }
