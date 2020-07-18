e42facff2d5166b90556190049f0dcf998eff03c
Jon Langlois <jonathan-langlois@live.ca>
2020-07-18 11:45:08 -0700

    add file compression

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const compression = require('compression');
 const redirectToHttps = require('express-http-to-https').redirectToHTTPS;
 const path = require('path')
 
 const app = express();
 
+app.use(compression())
 app.use(redirectToHttps());
 app.use(express.static(path.join(__dirname, 'build')));
 
