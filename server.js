1568bcf8bdf128582cacb95ceceecdaaa81cfb1c
Jon Langlois <jonathan-langlois@live.ca>
2020-07-18 11:32:32 -0700

    chagne buildpack

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(redirectToHttps());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(process.env.PORT || 8001, () => {
-  console.log('server running;')
+  console.log('server running')
 });
